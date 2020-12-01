b482aa6c331a653871ed40b6ce58198ecec7f7c3
Gfz-arka <74485508+Gfz-arka@users.noreply.github.com>
2020-12-02 01:00:08 +0800

    Update Test_strategy.py

--- Test_strategy.py
+++ Test_strategy.py
@@ -20,22 +20,22 @@ class TestStrategy(Strategy):
         self.bought = self._calculate_initial_bought()
 
     def _calculate_initial_bought(self):
-
+        # 创建初始的买入情况字典。所有的股票买入情况都为'OUT'，即未买入  
         bought = {}
         for s in self.symbol_list:
             bought[s] = 'OUT'
         return bought
 
     def calculate_stock_selection(self, cur_layer):
-
-        n = int(self.stock_num / self.layer)  # na值待处理
+        # 计算月底要买入哪些股票
+        n = int(self.stock_num / self.layer)  # 买入股票数量        ！！na值待处理！！
         bars = []
         for s in self.symbol_list:
             bars.append(self.bars.get_latest_bar_value(
-                s, self.factor))
+                s, self.factor))  # 将每支股票的因子放入列表中
         self.stock_list = pd.DataFrame(bars, self.symbol_list, columns=[self.factor])
-        self.stock_list = self.stock_list.sort_values(self.factor, ascending=False)
-        self.stock_list = self.stock_list[cur_layer * n: (cur_layer + 1) * n].reset_index()['index']
+        self.stock_list = self.stock_list.sort_values(self.factor, ascending=False)  # 按因子值排序
+        self.stock_list = self.stock_list[cur_layer * n: (cur_layer + 1) * n].reset_index()['index']  # 把前n支股票放入dataframe中，股票代码作为index
 
     def calculate_signals(self, event, cur_layer=0):
 
